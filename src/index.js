d6a534e279f3ba08cd9aa42a0ed84e1f03ab49ec
jerome creignou <j.creignou@bimedia.com>
2020-06-28 14:10:33 +0200

    fix config

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ module.exports = function setup(config, imports, done) {
 
     imports.hub.on('ready', (app) => {
         (config.services||[]).forEach((shortName) => {
-            let name = '_' + shortName + '._tcp.' + package.name + '.' + targetZoneName;
+            let name = '_' + shortName + '._tcp.' + config.name + '.' + targetZoneName;
             let port = app.services[shortName].address().port;
             log.info('registering service', name, 'has listening on', port, '@', exporter.fqdn);
             exporter.publish(name, port, config.weight || 33);
